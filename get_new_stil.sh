8ed672ab9afbf18193d450b748091d27aab0fb33
Norayr Chilingarian <norayr@arnet.am>
2026-04-23 23:11:54 +0000

    excalibur, better search.

---
+++ get_new_stil.sh
@@ -0,0 +1,1 @@
+wget -O STIL.txt https://www.hvsc.c64.org/download/C64Music/DOCUMENTS/STIL.txt
--- plugins/search/search_plugin_tp2b.cpp
+++ plugins/search/search_plugin_tp2b.cpp
@@ -1,134 +1,197 @@
-/*
- * Copyright (C) 2008 Tapani Pälli <lemody@c64.org>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- */
-
 #include "search_plugin_tp2b.h"
 #include "../curling.c"
 
-#define TMP_FILE "/tmp/sidweb_tmp.txt"
-#define SID_SERVER "http://hvsc.tp2.be/"
-#define BUFSIZE 256
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-static unsigned int
-parse_uri_from_sidsearch (const char *line, char*uri)
+#define TMP_FILE "/tmp/msid_search.json"
+#define TMP_DETAIL "/tmp/msid_detail.json"
+
+#define HVSC_API_SEARCH "https://www.hvsc.c64.org/api/v1/sids?q=%s"
+#define HVSC_API_DETAIL "https://www.hvsc.c64.org/api/v1/sids/%d"
+
+#define HVSC_MIRROR_BASE "https://hvsc.brona.dk/HVSC/C64Music"
+
+#define BUFSIZE 4096
+
+static gchar *
+json_extract_string(const gchar *json,
+                    const gchar *key)
 {
-  char *tmp, *end;
+  gchar pattern[128];
+  gchar *p;
+  gchar *start;
+  gchar *end;
 
-  uri[0] = '\0';
+  snprintf(pattern, sizeof(pattern), "\"%s\":\"", key);
 
-  if ((strstr(line, "&path=")) &&
-      (strstr(line, ".sid")))
-  {
-    // we have a hit
-    tmp = strstr (line, "&path=");
-    tmp += 6;
+  p = strstr(json, pattern);
 
-    end = strstr (tmp, ".sid");
-    end[4] = '\0';
+  if (!p)
+    return NULL;
 
-    snprintf (uri, BUFSIZE, "%s%s", SID_SERVER, tmp);
+  start = p + strlen(pattern);
+  end = strchr(start, '"');
 
-    return 1;
-  }
-  return 0;
+  if (!end)
+    return NULL;
+
+  return g_strndup(start, end - start);
 }
 
-static GSList*
-parse_uris_from_http_page (char *file_path)
+static int
+json_extract_id(const gchar *json)
 {
-  FILE *in;
-  GSList *list=NULL;
-  char buffer[BUFSIZE];
-  gchar uri[BUFSIZE];
+  gchar *p;
 
-  in = fopen (file_path, "r");
-  
-  if (in)
-  {
-    do
-    {
-      fgets (buffer, 256, in);
+  p = strstr(json, "\"id\":");
 
-      if (parse_uri_from_sidsearch (buffer, uri))
-      {
-	list = g_slist_prepend (list, g_strdup (uri));
-      }
+  if (!p)
+    return -1;
 
-    } while (!feof(in));
-    fclose (in);
-  }
-  return list;
+  p += 5;
+
+  return atoi(p);
 }
 
-char*
-get_filename_from_uri (char *uri)
+static gchar *
+read_file_to_string(const gchar *path)
 {
-  char *tmp = uri;
-  while (strstr (tmp, "/"))
-    tmp++;
-  return tmp;
+  FILE *f;
+  long len;
+  gchar *buf;
+
+  f = fopen(path, "r");
+
+  if (!f)
+    return NULL;
+
+  fseek(f, 0, SEEK_END);
+  len = ftell(f);
+  fseek(f, 0, SEEK_SET);
+
+  buf = (gchar *) malloc(len + 1);
+
+  fread(buf, 1, len, f);
+  buf[len] = '\0';
+
+  fclose(f);
+
+  return buf;
 }
 
-static gchar*
-make_hvsc_path (gchar *uri)
+static gchar *
+build_download_url(const gchar *filePath,
+                   const gchar *filename)
 {
-#define HVSC_ROOT "C64Music/"
-  char *p = strstr (uri, HVSC_ROOT);
-  if (p)
-  {
-    p += strlen (HVSC_ROOT + 1);
-    return g_strdup (p);
-  }
-  return NULL;
+  return g_strdup_printf("%s%s%s",
+                         HVSC_MIRROR_BASE,
+                         filePath,
+                         filename);
 }
-
 
 GSList *
-tp2b_search::search_for_sid (const char *needle,
-			     gpointer data)
+tp2b_search::search_for_sid(const char *needle,
+                            gpointer data)
 {
-  char buffer[256];
-  GSList *uri_list, *tmp;
   GSList *entry_list = NULL;
 
-  DEBUG ("searching for [%s] (%s)\n", needle, SID_SERVER);
+  gchar search_url[1024];
 
-  snprintf (buffer, 256, "%s?q=%s", SID_SERVER, needle);
+  gchar *json;
+  gchar *p;
 
-  fetch_data_to_file (buffer, TMP_FILE);
+  DEBUG("HVSC API search for [%s]\n", needle);
 
-  uri_list = parse_uris_from_http_page (TMP_FILE);
+  snprintf(search_url,
+           sizeof(search_url),
+           HVSC_API_SEARCH,
+           needle);
 
-  for (tmp=uri_list; tmp; tmp=g_slist_next(tmp))
+  fetch_data_to_file(search_url, TMP_FILE);
+
+  json = read_file_to_string(TMP_FILE);
+
+  if (!json)
+    return NULL;
+
+  p = json;
+
+  while ((p = strstr(p, "\"id\":")))
+  {
+    int id;
+
+    gchar detail_url[1024];
+
+    gchar *detail_json;
+
+    gchar *filePath;
+    gchar *filename;
+
+    msid_search_entry *entry;
+
+    id = json_extract_id(p);
+
+    if (id < 0)
     {
-      msid_search_entry *entry = (msid_search_entry *) malloc (sizeof (msid_search_entry));
-    
-      entry -> uri = g_strdup((char *) tmp->data);
-      entry -> file_name = get_filename_from_uri ((char *) entry->uri);
-      entry -> hvsc_path = make_hvsc_path (entry->uri);
-
-      entry_list = g_slist_prepend (entry_list, entry);
+      p += 5;
+      continue;
     }
 
-  g_slist_foreach (uri_list, (GFunc) g_free, NULL);
-  g_slist_free (uri_list);
+    snprintf(detail_url,
+             sizeof(detail_url),
+             HVSC_API_DETAIL,
+             id);
 
-  unlink (TMP_FILE);
+    fetch_data_to_file(detail_url, TMP_DETAIL);
+
+    detail_json = read_file_to_string(TMP_DETAIL);
+
+    if (!detail_json)
+    {
+      p += 5;
+      continue;
+    }
+
+    filePath = json_extract_string(detail_json, "filePath");
+    filename = json_extract_string(detail_json, "filename");
+
+    if (filePath && filename)
+    {
+      entry = (msid_search_entry *)
+        malloc(sizeof(msid_search_entry));
+
+      entry->uri =
+        build_download_url(filePath, filename);
+
+      entry->file_name =
+        g_strdup(filename);
+
+      entry->hvsc_path =
+        g_strdup_printf("%s%s",
+                         filePath,
+                         filename);
+
+      entry_list =
+        g_slist_prepend(entry_list, entry);
+    }
+
+    g_free(filePath);
+    g_free(filename);
+
+    free(detail_json);
+
+    p += 5;
+  }
+
+  free(json);
+
+  unlink(TMP_FILE);
+  unlink(TMP_DETAIL);
 
   return entry_list;
 }
+
